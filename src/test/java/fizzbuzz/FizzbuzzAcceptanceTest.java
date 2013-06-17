a7616ae09640da6e648cc89ec23a175fb578a14f
ITAGAKI, Shintaro <itagaki.shintaro@gmail.com>
2013-06-17 22:36:46 +0900

    cucumberのアノテーション追加

--- src/test/java/fizzbuzz/FizzbuzzAcceptanceTest.java
+++ src/test/java/fizzbuzz/FizzbuzzAcceptanceTest.java
@@ -2,33 +2,28 @@ package fizzbuzz;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 import cucumber.annotation.ja.*;
-import fizzbuzz.model.Fizzbuzz;
 import java.io.File;
-import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
-import org.junit.*;
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.Select;
+import cucumber.annotation.Before;
+import cucumber.annotation.After;
 
 public class FizzbuzzAcceptanceTest {
 	private WebDriver driver;
 	private String baseUrl;
-	private boolean acceptNextAlert = true;
 	private StringBuffer verificationErrors = new StringBuffer();
-	private String call, answer;
+	private String call;
 	private WebElement we;
 
 	@Before
 	public void setUp() throws Exception {
+		setWebdriver();
+	}
+
+	public void setWebdriver(){
 		FirefoxBinary binary = new FirefoxBinary(new File("D:\\Program Files\\Mozilla Firefox\\firefox.exe"));
 		FirefoxProfile profile = new FirefoxProfile();
 		driver = new FirefoxDriver(binary, profile);
@@ -49,84 +44,42 @@ public class FizzbuzzAcceptanceTest {
 
 	@もし("^\"([^\"]*)\"が３の倍数$")
 	public void が３の倍数(String call) throws Throwable {
-		setUp();
 		setInput(call);
 	}
 
 	@ならば("^answerはfizzであるべき$")
 	public void answerはfizzであるべき() throws Throwable {
 	    assertThat(we.getText(), is("答え：fizz"));
-	    tearDown();
 	}
 
 	@もし("^\"([^\"]*)\"が５の倍数$")
 	public void が５の倍数(String call) throws Throwable {
-		setUp();
 		setInput(call);
 	}
 
 	@ならば("^answerはbuzzであるべき$")
 	public void answerはbuzzであるべき() throws Throwable {
 		assertThat(we.getText(), is("答え：buzz"));
-	    tearDown();
 	}
 
 	@もし("^\"([^\"]*)\"が３の倍数かつ５の倍数$")
 	public void が３の倍数かつ５の倍数(String call) throws Throwable {
-		setUp();
 		setInput(call);
 	}
 
 	@ならば("^answerはfizzbuzzであるべき$")
 	public void answerはfizzbuzzであるべき() throws Throwable {
 		assertThat(we.getText(), is("答え：fizzbuzz"));
-	    tearDown();
 	}
 
 	@もし("^\"([^\"]*)\"が整数以外$")
 	public void が整数以外(String call) throws Throwable {
-		setUp();
 		setInput(call);
 	}
 
 	@ならば("^answerは\"([^\"]*)\"であるべき$")
 	public void answerは_であるべき(String arg1) throws Throwable {
 		assertThat(we.getText(), is("答え："+call));
-	    tearDown();
-	}
-
-
-	private boolean isElementPresent(By by) {
-		try {
-			driver.findElement(by);
-			return true;
-		} catch (NoSuchElementException e) {
-			return false;
-		}
-	}
-
-	private boolean isAlertPresent() {
-		try {
-			driver.switchTo().alert();
-			return true;
-		} catch (NoAlertPresentException e) {
-			return false;
-		}
-	}
-
-	private String closeAlertAndGetItsText() {
-		try {
-			Alert alert = driver.switchTo().alert();
-			String alertText = alert.getText();
-			if (acceptNextAlert) {
-				alert.accept();
-			} else {
-				alert.dismiss();
-			}
-			return alertText;
-		} finally {
-			acceptNextAlert = true;
-		}
 	}
 
 	@After
